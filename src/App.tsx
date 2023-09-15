88b31a578785531aa68a07230bc551a827157e1a
luciano.junior <luciano.junior@metareports.com.br>
2023-09-15 18:32:27 -0300

    commit de seguranca

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Github, FileVideo, Upload, Wand2 } from 'lucide-react'
+import { Github, Wand2 } from 'lucide-react'
 import { Button } from "./components/ui/button";
 import { Separator } from './components/ui/separator';
 import { Textarea } from './components/ui/textarea';
 import { Label } from './components/ui/label';
 import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem } from './components/ui/select';
 import { Slider } from './components/ui/slider';
+import { VideoInputForm } from './components/video-input-form';
+import { PromptSelect } from './components/prompt-select';
 
 export function App() {
+  function handlePromptSelected(template: string) {
+    console.log(template)
+  }
 
   return (
     <div className='min-h-screen flex flex-col'>
@@ -44,51 +49,14 @@ export function App() {
         </div>
 
         <aside className='w-80 space-y-6'>
-          <form className='space-y-6'>
-            <label 
-              htmlFor="video"
-              className='border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/10'
-            >
-              <FileVideo className='w-4 h-4'></FileVideo>
-              Selecione um vídeo
-            </label>
-
-            <input type="file" id='video' accept='video/mp4' className='sr-only'/>
-
-            <Separator />
-
-            <div className='space-y-2'>
-              <Label htmlFor='transcription_prompt'>
-                Prompt de transcrição
-              </Label>
-              <Textarea 
-                id="transcription_prompt" 
-                className='h-20 leading-relaxed resize-none'
-                placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula'                
-               />
-            </div>
-
-            <Button type='submit' className='w-full'>
-              Carregar vídeo
-              <Upload className='w-4 h-4 ml-2' />
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <Separator />
 
           <form className='space-y-6'>
             <div className='space-y-2'>
               <Label> Prompt </Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder='Selecione um prompt...' />
-                </SelectTrigger>
-
-                <SelectContent>
-                  <SelectItem value='title'>Título do youtube</SelectItem>
-                  <SelectItem value='description'>Descrição do youtube</SelectItem>
-                </SelectContent>
-              </Select>              
+             <PromptSelect onPromptSelected={handlePromptSelected} />           
             </div>
 
             <div className='space-y-2'>
